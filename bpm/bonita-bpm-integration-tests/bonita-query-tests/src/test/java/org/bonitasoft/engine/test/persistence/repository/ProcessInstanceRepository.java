a170d6f6aacb5f2156da95254b7f2ee00ef955df
julien-reboul <julien.reboul@bonitasoft.com>
2014-04-24 11:56:00 +0200

    add query tests for userWhoCanStartPendingTasks

--- bpm/bonita-bpm-integration-tests/bonita-query-tests/src/test/java/org/bonitasoft/engine/test/persistence/repository/ProcessInstanceRepository.java
+++ bpm/bonita-bpm-integration-tests/bonita-query-tests/src/test/java/org/bonitasoft/engine/test/persistence/repository/ProcessInstanceRepository.java
@@ -2,6 +2,7 @@ package org.bonitasoft.engine.test.persistence.repository;
 
 import java.util.List;
 
+import org.bonitasoft.engine.identity.model.SUser;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 
@@ -17,5 +18,18 @@ public class ProcessInstanceRepository extends TestRepository {
         Query namedQuery = getNamedQuery("getPossibleUserIdsOfPendingTasks");
         namedQuery.setParameter("humanTaskInstanceId", activityInstanceId);
         return namedQuery.list();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<SUser> searchPossibleUserIdsOfPendingTasks(long activityInstanceId) {
+        Query namedQuery = getNamedQuery("searchSUserWhoCanStartPendingTask");
+        namedQuery.setParameter("humanTaskInstanceId", activityInstanceId);
+        return namedQuery.list();
+    }
+
+    public long getNumberOfSUserWhoCanStartPendingTask(long activityInstanceId) {
+        Query namedQuery = getNamedQuery("getNumberOfSUserWhoCanStartPendingTask");
+        namedQuery.setParameter("humanTaskInstanceId", activityInstanceId);
+        return ((Number) namedQuery.uniqueResult()).longValue();
     }
 }
